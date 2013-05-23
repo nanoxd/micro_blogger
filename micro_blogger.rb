23aebd9f3c6b1c3690ce51e5317d91e8d1b029a7
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-23 13:19:35 -0700

    Add tweeting capabilities

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -4,6 +4,19 @@ class MicroBlogger
   attr_reader :client
 
   def initialize
-    puts "Initializing
+    puts "Initializing"
+    @client = JumpstartAuth.twitter
   end
+
+  def tweet(message)
+    if message.length <= 140
+      @client.update(message)
+    else
+      "The post is too long
+    end
+  end
+
 end
+
+blogger = MicroBlogger.new
+blogger.tweet("Gem Initialized")
